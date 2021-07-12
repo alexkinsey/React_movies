2edae6f35f8628e8b0407d02bcfec0fc0eb86283
alexkinsey <alexanderkinsey@icloud.com>
2021-07-12 14:49:05 +0100

    fixed api

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // Read more about the API here: https://developers.themoviedb.org/
 
 const API_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = '451dae7d75c17710dc61cc9a281ae7aa';
 
 const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=`;
 const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`;
